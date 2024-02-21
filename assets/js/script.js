d736c5b32b4ee8dacedb05310ca0a797eea0c7b2
Floris <Florismelchers@gmail.com>
2024-02-21 23:15:05 +0100

    started on portfolio part

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
+//navigation
 function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
+  var x = document.getElementById("nav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
   }
+};
